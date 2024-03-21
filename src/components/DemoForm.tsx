0979d1721f91f07b6d66e9002cb3de173c90760c
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2024-03-22 01:36:42 +0600

    more validation and fields added

--- src/components/DemoForm.tsx
+++ src/components/DemoForm.tsx
@@ -1,20 +1,44 @@
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
 type formValues = {
   username: string;
   email: string;
   channel: string;
+  social: {
+    facebook: string;
+    twitter: string;
+  };
+  phoneNumbers: { number: string }[];
+  age: number;
+  dob: Date;
 };
 
 const DemoForm = () => {
-  const form = useForm<formValues>();
+  const form = useForm<formValues>({
+    defaultValues: {
+      username: '',
+      email: '',
+      channel: '',
+      social: { facebook: '', twitter: '' },
+      phoneNumbers: [{ number: '' }],
+      age: 0,
+      dob: new Date(),
+    },
+  });
   const {
     register,
     control,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, dirtyFields, },
   } = form;
+
+  console.log();
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'phoneNumbers',
+    control,
+  });
 
   const onSubmit = (data: formValues) => {
     console.log(data);
@@ -41,6 +65,7 @@ const DemoForm = () => {
             type="email"
             id="email"
             {...register('email', {
+              required: { value: true, message: 'Email is required' },
               pattern: {
                 value:
                   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -67,10 +92,85 @@ const DemoForm = () => {
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" {...register('channel')} />
+          <input
+            type="text"
+            id="channel"
+            {...register('channel', {
+              required: { value: true, message: 'channel is required!' },
+            })}
+          />
           <p className="error">{errors.channel?.message}</p>
         </div>
-        <button>submit</button>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <input type="text" id="facebook" {...register('social.facebook')} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register('social.twitter')} />
+        </div>
+
+        <div>
+          <label htmlFor="phoneNumber">Phone Numbers List</label>
+          <div>
+            {fields.map((field, index) => (
+              <div className="form-control" key={field.id}>
+                <input
+                  placeholder={`phone number ${index + 1}`}
+                  type="text"
+                  id="phoneNumber"
+                  {...register(`phoneNumbers.${index}.number`)}
+                />
+                {index > 0 && (
+                  <button
+                    className="btn"
+                    type="button"
+                    onClick={() => remove(index)}
+                  >
+                    Delete
+                  </button>
+                )}
+              </div>
+            ))}
+            <button
+              className="btn"
+              type="button"
+              onClick={() => append({ number: '' })}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            {...register('age', {
+              valueAsNumber: true,
+              required: { value: true, message: 'Age is required!' },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="dob">Date of birth</label>
+          <input
+            type="date"
+            id="dob"
+            {...register('dob', {
+              valueAsDate: true,
+              required: { value: true, message: 'Date of birth is required!' },
+            })}
+          />
+          <p className="error">{errors.age?.message}</p>
+        </div>
+
+        <button className="btn">submit</button>
       </form>
       <DevTool control={control} />
     </div>
